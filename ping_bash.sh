079713a6a163e6ea9bd223dc7d5c0c2e0226ec32
Sigri44 <sigri44@hotmail.fr>
2018-10-31 14:39:26 +0100

    Format Integer + Clean + ShellCheck

--- ping_bash.sh
+++ ping_bash.sh
@@ -1,33 +1,23 @@
+#!/bin/bash
 machine="www.google.fr"
 coupures=0
 while true;
 do
-  PING=`ping -c 1 $machine`
-  if [[ $? -eq 0 ]] ; then
-    CMD_DATE=`date`
-    DATE=`echo $CMD_DATE | tail -1 | cut -c 17-24`
-    TIME=`echo $PING | tail -1 | cut -d/ -f 5`
-    if [[ ${TIME} < 20 ]] ; then
-        echo ${DATE} : ping sur $machine : `echo -e "\033[30m\033[42m${TIME} ms\033[0m"`
-    elif ([ ${TIME} > 20 ] && [ ${TIME} < 99 ]) ; then
-        echo ${TIME}
-        echo ${DATE} : ping sur $machine : `echo -e "\033[30m\033[43m${TIME} ms\033[0m"`
-    elif [[ ${TIME} > 99 ]] ; then
-        echo ${TIME}
-        echo ${DATE} : ping sur $machine : `echo -e "\033[30m\033[41m${TIME} ms\033[0m"`
+  CMD_DATE=$(date)
+  DATE=$(echo "$CMD_DATE" | tail -1 | cut -c 17-24)
+  PING=$(ping -c 1 $machine)
+  if [ "$PING" ] ; then
+    TIME=$(echo "$PING" | tail -1 | cut -d/ -f 5 | cut -d. -f 1)
+    if [ "${TIME}" -lt 20 ] ; then
+        echo "${DATE}" : ping sur $machine : "$(echo -e "\033[30m\033[42m${TIME} ms\033[0m")"
+    elif [ "${TIME}" -gt 20 ] && [ "${TIME}" -lt 99 ]  ; then
+        echo "${DATE}" : ping sur $machine : "$(echo -e "\033[30m\033[43m${TIME} ms\033[0m")"
     else
-        echo Erreur non référencée
+        echo "${DATE}" : ping sur $machine : "$(echo -e "\033[30m\033[41m${TIME} ms\033[0m")"
     fi
     sleep 1
   else
-    #$coupures = $coupures + 1
-    $coupures = $coupures + 1
-    #var=$((var + 1))
-
-    CMD_DATE=`date`
-    DATE=`echo $CMD_DATE | tail -1 | cut -c 17-24`
-    echo ${DATE} : Coupure réseau - Impossible de joindre la machine $machine
-    echo ${DATE} : Nb de coupures réseau : $coupures
+    $coupures++
+    echo "${DATE} : Coupure réseau détectée - (Nb de coupures réseau : $coupures)"
   fi
-done
 done
